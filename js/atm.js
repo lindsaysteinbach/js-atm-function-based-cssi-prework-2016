c37d87d9a20e71eaecf760c5babd5f770443e3d2
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-17 17:35:52 -0400

    boilerplate for spec:

--- js/atm.js
+++ js/atm.js
@@ -2,4 +2,8 @@ $(function(){
 
   //start coding here...
 
+  $("#submit").click(function(e){
+    // we need this to prevent the form for resubmitting, specifically on the specrunner.html (we have our project's html in here so we don't have to include fixtures in the spec)
+    e.preventDefault();
+  })
 });
---
+++ spec/atm_spec.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  describe("Completing a transaction", function() {
+      
+    xit ("should validate that a user's input is a number", function() {
+
+    });
+
+    xit ("should add the amount if the user selects deposit", function() {
+
+    });
+
+    xit ("should subtract the amount if the user selects withdrawl", function() {
+
+    });
+
+    xit ("should display the user's current balance after deposit/withdrawl actions", function() {
+
+    });
+  
+  });
+
+});
