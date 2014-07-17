75c2311e6476e7b1c3116d26aa36f99dafa1c1e2
ahimmelstoss <ahimmelstoss@gmail.com>
2014-07-17 18:29:28 -0400

    tests

--- spec/atm_spec.js
+++ spec/atm_spec.js
@@ -1,21 +1,35 @@
 $(document).ready(function(){
 
   describe("Completing a transaction", function() {
+
+    it ("should hide the submit button and input field before deposit or withdrawl is selected", function() {
+      expect($('#amount')).toBeHidden();
+    });
       
-    xit ("should validate that a user's input is a number", function() {
-
+    it ("should validate that a user's input isn't empty", function() {
+      $('#amount').val("");
+      $("[type='submit']").click();
+      expect($('#balance').text()).toBe('Fool don\'t be playin!');
     });
 
-    xit ("should add the amount if the user selects deposit", function() {
-
+    it ("should validate that a user's input is a number", function() {
+      $('#amount').val(NaN);
+      $("[type='submit']").click();
+      expect($('#balance').text()).toBe('Fool don\'t be playin!');
     });
 
-    xit ("should subtract the amount if the user selects withdrawl", function() {
-
+    it ("should add and display the amount if the user selects deposit", function() {
+      $('#choice').val('deposit').change();
+      $('#amount').val(100);
+      $("[type='submit']").click();
+      expect($('#balance').text()).toBe('balance: $100');
     });
 
-    xit ("should display the user's current balance after deposit/withdrawl actions", function() {
-
+    it ("should subtract and display the amount if the user selects withdrawl", function() {
+      $('#choice').val('withdrawal').change();
+      $('#amount').val(100);
+      $("[type='submit']").click();
+      expect($('#balance').text()).toBe('balance: $0');
     });
   
   });
