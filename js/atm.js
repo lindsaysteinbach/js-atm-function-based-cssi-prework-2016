805cbf89fc050367181b86df1be1e198300db736
lindsaysteinbach <lsteinbach@google.com>
2016-05-16 10:09:32 -0400

    Done.

--- js/atm.js
+++ js/atm.js
@@ -1,6 +1,35 @@
 $(function(){
 
-  //start coding here...
+$('#amount').hide();
+$('#submit').hide();
+
+
+$('#choice').change(function(e){
+  if ($(this).val()=="-- select choice --") {
+    $('#amount').hide();
+    $('#submit').hide();
+  }
+  else {
+    $('#amount').show();
+    $('#submit').show();
+  }
+})
+
+var balance = 0;
+
+  $("#submit").click(function(e){
+    var entry = $('#amount').val();
+
+    if ($('#choice').val() == "deposit") {
+      balance = balance + parseFloat(entry);
+      $('#balance').text("balance: $" + balance);
+
+    }
+    else if ($('#choice').val() == "withdrawal") {
+      balance = balance - parseFloat(entry);
+      $('#balance').text("balance: $" + balance);
+    }
+  })
 
   $("#submit").click(function(e){
     // we need this to prevent the form for resubmitting, specifically on the specrunner.html (we have our project's html in here so we don't have to include fixtures in the spec)
